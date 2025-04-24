6ce8727913a741b782fe5993adca622f9e1193f9
Code-Commit-at-851725190791 <hariprasadjkclass11thb@gmail.com>
2025-04-24 17:55:24 +0530

    new update

--- src/pages/Journy/Journey.js
+++ src/pages/Journy/Journey.js
@@ -181,6 +181,174 @@
 //     );
 // }
 
+// // export default Journey;
+// import React, { useState, useEffect } from 'react';
+// import './Journey.css';
+// import Navi from '../home/nav/Navi';
+// import axios from 'axios';
+
+// function Journey() {
+//     // const [user_id, setUserid] = useState('');
+//     const [showModal, setShowModal] = useState(false);
+//     const [selectedTrip, setSelectedTrip] = useState(null);
+//     const [trips, setTrips] = useState([]);
+//     const [formData, setFormData] = useState({
+//         username: '',
+//         usermail: '',
+//         userReview: '',
+//         starrating: 0,
+//     });
+//     const [showThankYou, setShowThankYou] = useState(false);
+
+//     useEffect(() => {
+//         const fetchUserAndTrips = async () => {
+//             const storedUser = JSON.parse(localStorage.getItem('user'));
+//             if (storedUser) {
+//                 setUserid(storedUser.id || '');
+//                 try {
+//                     const response = await axios.get(`http://localhost:9001/bookings/byUserId/${storedUser.id}`);
+//                     setTrips(response.data);
+//                 } catch (error) {
+//                     console.error('There was an error getting the trips!', error);
+//                 }
+//             }
+//         };
+    
+//         fetchUserAndTrips();
+//     }, []);
+    
+
+//     const openModal = (trip) => {
+//         setSelectedTrip(trip);
+//         setShowModal(true);
+//     };
+
+//     const closeModal = () => {
+//         setShowModal(false);
+//         setFormData({
+//             starrating: 0,
+//             userReview: '',
+//             usermail: '',
+//             username: '',
+//         });
+//         setShowThankYou(false);
+//     };
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         try {
+//             await axios.post("http://localhost:9001/review/postreview", formData);
+//             setShowThankYou(true);
+//             setTimeout(() => {
+//                 closeModal();
+//             }, 3000);
+//         } catch (error) {
+//             console.error("Error while uploading the review", error);
+//         }
+//     };
+
+//     const handleInputChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData({ ...formData, [name]: value });
+//     };
+
+//     const handleRatingChange = (rating) => {
+//         setFormData({ ...formData, starrating: rating });
+//     };
+
+//     return (
+//         <div>
+//             <Navi />
+//             <div className='oke'>
+//                 <div className='ok1'> 
+//                     <h2 className='head1'>Trips Completed</h2>
+//                     <div className='ol21'>
+//                         {trips.map((trip) => (
+//                             <div key={trip.id} className="card1">
+//                                 <div className='ioio'>
+//                                 <img src={trip.img} alt={trip.name} />
+//                                 <div className='ioi'>
+//                                 <h5>Days : {trip.numDays}</h5>
+//                                 <h5>Amount : ₹{trip.totalPrice}</h5>
+//                                 </div></div>
+//                                 <h5>Pickup Date : {trip.pickDate}</h5>
+//                                 <h5>Drop Date : {trip.dropDate}</h5>
+//                                 <div className="details1">
+//                                     <button onClick={() => openModal(trip)}>Review</button>
+//                                 </div>
+//                             </div>
+//                         ))}
+//                     </div>
+//                 </div>
+//             </div>
+
+//             {showModal && (
+//                 <div className="modal">
+//                     <div className="modal-content">
+//                         {showThankYou ? (
+//                             <div>
+//                                 <h2>Thank you for your feedback!</h2>
+//                                 <div className='pattu'>
+//                                     <button onClick={closeModal}>Close</button>
+//                                 </div>
+//                             </div>
+//                         ) : (
+//                             <form onSubmit={handleSubmit}>
+//                                 <h2>Leave a Review for {selectedTrip.name}</h2>
+//                                 <label>
+//                                     <p>Name:</p>
+//                                     <input
+//                                         type="text"
+//                                         name="username"
+//                                         value={formData.username}
+//                                         onChange={handleInputChange}
+//                                     />
+//                                 </label>
+//                                 <label>
+//                                     <p>Email:</p>
+//                                     <input
+//                                         type="email"
+//                                         name="usermail"
+//                                         value={formData.usermail}
+//                                         onChange={handleInputChange}
+//                                     />
+//                                 </label>
+//                                 <label>
+//                                     <p>Review:</p>
+//                                     <textarea
+//                                         name="userReview"
+//                                         value={formData.userReview}
+//                                         onChange={handleInputChange}
+//                                     />
+//                                 </label>
+//                                 <label>
+//                                     <div>
+//                                         <p>Rate The Trip</p>
+//                                         <div className="star-rating">
+//                                             {Array.from({ length: 5 }, (_, i) => (
+//                                                 <span
+//                                                     key={i}
+//                                                     className={i < formData.starrating ? 'star filled' : 'star'}
+//                                                     onClick={() => handleRatingChange(i + 1)}
+//                                                 >
+//                                                     ★
+//                                                 </span>
+//                                             ))}
+//                                         </div>
+//                                     </div>
+//                                 </label>
+//                                 <div className='pattu'>
+//                                     <button type="submit">Submit</button>
+//                                 </div>
+//                             </form>
+//                         )}
+//                     </div>
+//                 </div>
+//             )}
+//         </div>
+//     );
+// }
+
 // export default Journey;
 import React, { useState, useEffect } from 'react';
 import './Journey.css';
@@ -188,7 +356,6 @@ import Navi from '../home/nav/Navi';
 import axios from 'axios';
 
 function Journey() {
-    // const [user_id, setUserid] = useState('');
     const [showModal, setShowModal] = useState(false);
     const [selectedTrip, setSelectedTrip] = useState(null);
     const [trips, setTrips] = useState([]);
@@ -204,7 +371,6 @@ function Journey() {
         const fetchUserAndTrips = async () => {
             const storedUser = JSON.parse(localStorage.getItem('user'));
             if (storedUser) {
-                setUserid(storedUser.id || '');
                 try {
                     const response = await axios.get(`http://localhost:9001/bookings/byUserId/${storedUser.id}`);
                     setTrips(response.data);
@@ -216,7 +382,6 @@ function Journey() {
     
         fetchUserAndTrips();
     }, []);
-    
 
     const openModal = (trip) => {
         setSelectedTrip(trip);
@@ -266,11 +431,12 @@ function Journey() {
                         {trips.map((trip) => (
                             <div key={trip.id} className="card1">
                                 <div className='ioio'>
-                                <img src={trip.img} alt={trip.name} />
-                                <div className='ioi'>
-                                <h5>Days : {trip.numDays}</h5>
-                                <h5>Amount : ₹{trip.totalPrice}</h5>
-                                </div></div>
+                                    <img src={trip.img} alt={trip.name} />
+                                    <div className='ioi'>
+                                        <h5>Days : {trip.numDays}</h5>
+                                        <h5>Amount : ₹{trip.totalPrice}</h5>
+                                    </div>
+                                </div>
                                 <h5>Pickup Date : {trip.pickDate}</h5>
                                 <h5>Drop Date : {trip.dropDate}</h5>
                                 <div className="details1">
--- src/pages/admin/Side.js
+++ src/pages/admin/Side.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import {  useNavigate} from 'react-router-dom';
-import './Side.css'; // Import the CSS file for the sidebar
-import { GrFormView } from "react-icons/gr";
-const Side = () => {
+// import React from 'react';
+// import {  useNavigate} from 'react-router-dom';
+// import './Side.css'; // Import the CSS file for the sidebar
+// import { GrFormView } from "react-icons/gr";
+// const Side = () => {
   
-const navi=useNavigate('');
+// const navi=useNavigate('');
+//   return (
+//     <div className="sidebar">
+//       <h2>Rent'N Roll</h2>
+//       <div className='hue'>
+//       <button  onClick={()=>{navi('/caradd')}}>Add Cars</button>
+//       <button  onClick={()=>{navi('/carview')}}>View Cars</button>
+//       <button  onClick={()=>{navi('/editcar')}}>Edit Car</button>
+//       <button  onClick={()=>{navi('/removecar')}}>Remove Car</button>
+//       <button  onClick={()=>{navi('/reviews')}}>Reviews</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Side;
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Side.css'; // Import the CSS file for the sidebar
+
+const Side = () => {
+  const navi = useNavigate(); // Using the useNavigate hook
+
   return (
     <div className="sidebar">
       <h2>Rent'N Roll</h2>
       <div className='hue'>
-      <button  onClick={()=>{navi('/caradd')}}>Add Cars</button>
-      <button  onClick={()=>{navi('/carview')}}>View Cars</button>
-      <button  onClick={()=>{navi('/editcar')}}>Edit Car</button>
-      <button  onClick={()=>{navi('/removecar')}}>Remove Car</button>
-      <button  onClick={()=>{navi('/reviews')}}>Reviews</button>
+        <button onClick={() => { navi('/caradd') }}>Add Cars</button>
+        <button onClick={() => { navi('/carview') }}>View Cars</button>
+        <button onClick={() => { navi('/editcar') }}>Edit Car</button>
+        <button onClick={() => { navi('/removecar') }}>Remove Car</button>
+        <button onClick={() => { navi('/reviews') }}>Reviews</button>
       </div>
     </div>
   );
